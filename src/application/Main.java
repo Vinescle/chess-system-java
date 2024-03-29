5472461ce6b32df02ef00358f8425f87ef183103
Vini <Vinicius.bmartinho40@gmail.com>
2024-03-29 20:29:02 -0300

    Movimento especial Promoção

--- src/application/Main.java
+++ src/application/Main.java
@@ -39,6 +39,13 @@ public class Main {
 				if (capturedPiece != null) {
 					captured.add(capturedPiece);
 				}
+				
+				if (chessMatch.getPromoted() != null) {
+					System.out.print("Selecione uma peca para promocao (B/N/R/Q): ");
+					String type = sc.nextLine();
+					chessMatch.replacePromotedPiece(type);
+				}
+				
 			} catch (ChessException e) {
 				System.out.println(e.getMessage());
 				sc.nextLine();
